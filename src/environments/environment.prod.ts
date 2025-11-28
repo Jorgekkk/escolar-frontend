a7612c9c325b180c4e2791ad68c42a77ec327c4b
Jorgekkk <142845784+Jorgekkk@users.noreply.github.com>
2025-11-28 16:17:43 -0600

    Agregando fileReplacements para entornos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url_api: 'https://escolar-backend.onrender.com/' // <--- PEGA AQUÍ LA URL DE RENDER
+  url_api: 'https://escolar-backend.onrender.com' // <--- PEGA AQUÍ LA URL DE RENDER
 };
